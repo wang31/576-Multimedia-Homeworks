1159705908dec9ca51fcd68c961a43ce5f467a9b
ZWang <hyopalexus@gmail.com>
2014-02-17 07:42:39 -0800

    animation done

--- hw/hw1/code/ImageReader_Java/imageReader.java
+++ hw/hw1/code/ImageReader_Java/imageReader.java
@@ -1,16 +1,24 @@
-
+/*
+* Zheng Wang's cs576 hw1
+*
+* */
 import java.awt.*;
 import java.awt.image.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.swing.*;
+import javax.swing.ImageIcon;
+import java.lang.*;
 import java.lang.Integer;
+import java.lang.InterruptedException;
 import java.lang.Math;
 import java.lang.System;
-
+import java.lang.Thread;
 
 public class imageReader {
 	private static final int width = 512, height = 512;
+    private static byte[] bytes = null;
+    private static BufferedImage[] animationFrames = null;
 	//only for 2 by 2 matrix
 	private static void inverseMatrix(float[][] m){
 		float a = m[0][0];
@@ -43,24 +51,40 @@ public class imageReader {
         return new float[]{x_origin + width / 2, y_origin + height / 2};
     }
 
-   //display the original and the modified image
-   private static void displayImages(String filename, float scale, float rotation, int antialiasing){
-       BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-       BufferedImage img_modified = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
+   private static void displayImage(BufferedImage image){
+       if(image != null){
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+       }
+   }
+
+   private static void readFromFile(String filename){
        try {
-           File file = new File(filename);
-           InputStream is = new FileInputStream(file);
+            File file = new File(filename);
+            InputStream is = new FileInputStream(file);
 
-           long len = file.length();
-           byte[] bytes = new byte[(int)len];
+            long len = file.length();
+            bytes = new byte[(int)len];
 
-           int offset = 0;
-           int numRead = 0;
-           while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+            int offset = 0;
+            int numRead = 0;
+            while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
                offset += numRead;
-           }
+            }
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
 
+   private static void displayOriginalImage(){//to display original image
+       if(bytes != null){
+           BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
            //for the original image
            int ind = 0;
            for(int y = 0; y < height; y++){
@@ -78,43 +102,89 @@ public class imageReader {
                    ind++;
                }
            }
+           displayImage(img);
+        }
+   }
 
-           //for modified image
-           for(int y = 0; y < height; y++){
-
-               for(int x = 0; x < width; x++){
-                   int[] coordinate = new int[2];
-                   coordinate[0] = x;
-                   coordinate[1] = y;
-                   float[] coord_float = inverseTransform(coordinate, scale, rotation);
-                   if(coordinate[0] >= 0 && coordinate[1] >= 0 && coordinate[0] < width - 1 && coordinate[1] < height - 1){
-                       if(antialiasing == 0){
-                           ind = coordinate[0] + coordinate[1] * width;
-                           byte a = 0;
-                           byte r = bytes[ind];
-                           byte g = bytes[ind+height*width];
-                           byte b = bytes[ind+height*width*2];
-
-                           int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-                           //int pix = ((a << 24) + (r << 16) + (g << 8) + b);
-                           img_modified.setRGB(x,y,pix);
-                       }
-                       else if(antialiasing == 1){//does antialiasing using bilinear interpolation
+   private static void constructAnimationFrames(float scale, float rotation, int antialiasing, int framerate, int time){
+       if(bytes != null){
+           int numberOfFrames = framerate * time;
+           animationFrames = new BufferedImage[numberOfFrames];
+           if(scale >= 1.0f){
+                for(int i = 0; i < numberOfFrames; i++){
+                   animationFrames[i] = constructModifiedImage(((scale - 1.0f) / numberOfFrames * i) + 1.0f, rotation / numberOfFrames * i, antialiasing);
+                }
+           }
+           else if(scale > 0.0f){
+               for(int i = 0; i < numberOfFrames; i++){
+                   animationFrames[i] = constructModifiedImage(1.0f - ((1.0f - scale) / numberOfFrames * i), rotation / numberOfFrames * i, antialiasing);
+               }
+           }
+       }
+   }
 
 
-                           int ind_topleft = coordinate[0] + coordinate[1] * width;
-                           int ind_topright = ind_topleft + 1;
-                           int ind_bottomleft = ind_topleft + width;
-                           int ind_bottomright = ind_bottomleft + 1;
+   private static void playAnimation(int framerate){
+       float interval = 1.0f / framerate;
+       interval *= 1000000000;
+       JFrame frame = new JFrame();
+       JLabel label = new JLabel(new ImageIcon(animationFrames[0]));
+       frame.getContentPane().add(label, BorderLayout.CENTER);
+       frame.pack();
+       frame.setVisible(true);
+       long time = System.nanoTime();
+       int i = 1;
+       while(i < animationFrames.length){
+           if(System.nanoTime() - time > interval){
+               label.setIcon(new ImageIcon(animationFrames[i]));
+                i++;
+                time = System.nanoTime();
+           }
+       }
+   }
+   //display the original and the modified image
+   private static BufferedImage constructModifiedImage(float scale, float rotation, int antialiasing){
+
+       if(bytes != null && scale > 0.0f){
+           BufferedImage img_modified = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+       int ind = 0;
+       //for modified image
+       for(int y = 0; y < height; y++){
+
+            for(int x = 0; x < width; x++){
+                int[] coordinate = new int[2];
+                coordinate[0] = x;
+                coordinate[1] = y;
+                float[] coord_float = inverseTransform(coordinate, scale, rotation);
+                if(coordinate[0] >= 0 && coordinate[1] >= 0 && coordinate[0] < width - 1 && coordinate[1] < height - 1){
+                    if(antialiasing == 0){
+                        ind = coordinate[0] + coordinate[1] * width;
+                        byte a = 0;
+                        byte r = bytes[ind];
+                        byte g = bytes[ind+height*width];
+                        byte b = bytes[ind+height*width*2];
+
+                        int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+                        //int pix = ((a << 24) + (r << 16) + (g << 8) + b);
+                        img_modified.setRGB(x,y,pix);
+                    }
+                    else if(antialiasing == 1){//does antialiasing using bilinear interpolation
 
 
-                           //interpolation of the r value
-                           Byte b1 = bytes[ind_topleft];
+                        int ind_topleft = coordinate[0] + coordinate[1] * width;
+                        int ind_topright = ind_topleft + 1;
+                        int ind_bottomleft = ind_topleft + width;
+                        int ind_bottomright = ind_bottomleft + 1;
+
+
+                        //interpolation of the r value
+                        Byte b1 = bytes[ind_topleft];
                            Byte b2 = bytes[ind_topright];
                            int b1_int;
                            int b2_int;
                            if(b1.intValue() < 0){
-                               b1_int = b1.intValue() + 256;
+                               b1_int = b1.intValue() + 256;//convert to unsigned value
                            }
                            else{
                                b1_int = b1.intValue();
@@ -239,33 +309,29 @@ public class imageReader {
                    }
                }
            }
-
-       } catch (FileNotFoundException e) {
-           e.printStackTrace();
-       } catch (IOException e) {
-           e.printStackTrace();
+           return img_modified;
        }
+       else return null;
 
-       // Use a label to display the image
-       JFrame frame = new JFrame();
-       JLabel label = new JLabel(new ImageIcon(img));
-       frame.getContentPane().add(label, BorderLayout.CENTER);
-       frame.pack();
-       frame.setVisible(true);
-
-       JFrame frame1 = new JFrame();
-       JLabel label1 = new JLabel(new ImageIcon(img_modified));
-       frame1.getContentPane().add(label1, BorderLayout.CENTER);
-       frame1.pack();
-       frame1.setVisible(true);
    }
+
 
     public static void main(String[] args) {
 	    String fileName = args[0];
+        readFromFile(fileName);
 	    float scale = Float.parseFloat(args[1]);
         float rotation = Float.parseFloat(args[2]);
         int antialiasing = Integer.parseInt(args[3]);
-        displayImages(fileName, scale, rotation, antialiasing);
+        if(args.length == 6){
+            int framerate = Integer.parseInt(args[4]);
+            int seconds = Integer.parseInt(args[5]);
+            constructAnimationFrames(scale, rotation, antialiasing, framerate, seconds);
+            playAnimation(framerate);
+        }
+        else{
+            displayOriginalImage();
+            displayImage(constructModifiedImage(scale, rotation, antialiasing));
+        }
    }
   
 }
